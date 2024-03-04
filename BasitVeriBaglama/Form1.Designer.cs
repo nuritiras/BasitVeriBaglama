d34db190fabe62cfa847a6253e6f176474a784e8
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-03-04 09:59:43 +0300

    Add project files.

---
+++ BasitVeriBaglama/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace BasitVeriBaglama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textKaynak = new System.Windows.Forms.TextBox();
+            this.textHedef = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kaynak";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hedef";
+            // 
+            // textKaynak
+            // 
+            this.textKaynak.Location = new System.Drawing.Point(103, 28);
+            this.textKaynak.Name = "textKaynak";
+            this.textKaynak.Size = new System.Drawing.Size(143, 20);
+            this.textKaynak.TabIndex = 2;
+            // 
+            // textHedef
+            // 
+            this.textHedef.Location = new System.Drawing.Point(103, 62);
+            this.textHedef.Name = "textHedef";
+            this.textHedef.Size = new System.Drawing.Size(143, 20);
+            this.textHedef.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 174);
+            this.Controls.Add(this.textHedef);
+            this.Controls.Add(this.textKaynak);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textKaynak;
+        private System.Windows.Forms.TextBox textHedef;
+    }
+}
+
